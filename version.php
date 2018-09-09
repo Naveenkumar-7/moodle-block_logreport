6d0c554333836719d03365a4f508342295d382fd
MNaveenkumar <naveen@eabyas.in>
2018-09-09 21:53:01 +0530

    Updated maturity and required version

--- version.php
+++ version.php
@@ -24,4 +24,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_logreport';
+$plugin->requires = 2016120509;
+$plugin->maturity   = MATURITY_STABLE;
+$plugin->release    = '3.2.9';
 $plugin->version = 2018061000;
