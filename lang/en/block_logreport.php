4784c3e99ce697f869fbe19414ae5f2452908a23
Naveen kumar <naveen@eabyas.in>
2019-01-02 20:43:39 +0530

    Update block_logreport.php

--- lang/en/block_logreport.php
+++ lang/en/block_logreport.php
@@ -31,3 +31,4 @@ $string['viewreport'] = 'View report';
 $string['clear'] = 'Reset';
 $string['logreport:addinstance'] = 'Add log report block';
 $string['logreport:myaddinstance'] = 'Add the block to my dashboard';
+$string['privacy:metadata'] = 'The Log report block only displays existing log data.';
